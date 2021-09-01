25728d9765054e02fb04c2dda4f8cb1e083081ef
Björn Bosse <bjoern.bosse@benntec.de>
2021-09-01 15:52:05 +0200

    Added icon for list delete button

--- client/src/board/List.js
+++ client/src/board/List.js
@@ -43,7 +43,9 @@ export default class List extends Component {
             <div key={l.id} id={l.id} className="list">
                 <div className="titlebar">
                     <EditableLabel text={l.title} placeholder="Add a title..." submit={(text) => this.props.updateListTitle(l.id, text)} />
-                    <div className="delete-list" onClick={() => this.props.deleteList(l.id)}>X</div>
+                    <div className="delete-list" onClick={() => this.props.deleteList(l.id)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#333333AA"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/></svg>
+                    </div>
                     <div className="task-count"><span>{l.cards.length}</span></div>
                 </div>
 
