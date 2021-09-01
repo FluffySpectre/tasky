457fc02688a1ebe3dc584e9a93b8045937673625
Björn Bosse <b.bosse1991@gmail.com>
2021-09-01 22:34:30 +0200

    Changed some icons and layout fixes

--- client/src/board/EditCardDialog.js
+++ client/src/board/EditCardDialog.js
@@ -7,8 +7,8 @@ export default class EditCardDialog extends React.Component {
     super(props);
 
     this.state = {
-      title: props.cardTitle,
-      description: props.cardDescription,
+      title: props.cardTitle || '',
+      description: props.cardDescription || '',
     };
   }
 
@@ -17,15 +17,23 @@ export default class EditCardDialog extends React.Component {
   }
 
   handleSubmit = () => {
+    if (!this.validTitle()) return;
     this.props.edit(this.state.title.trim(), this.state.description.trim());
   }
+
+  validTitle() {
+    const t = this.state.title.replace('\n', '');
+    return t && t.length > 0;
+}
 
   render() {
     return (
       <div className="edit-card-dialog">
         <div className="backdrop"></div>
         <div className="dialog-container">
-          <div className="dialog-close" onClick={this.props.cancel}><span>X</span></div>
+          <div className="dialog-close" onClick={this.props.cancel}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#333333AA"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
+          </div>
           <EditableLabel text={this.state.title} className="dialog-title" placeholder="Add a title..." submit={(text) => this.setState({ title: text })} />
           <textarea className="dialog-description-input" value={this.state.description} placeholder="A description can be put here..." onChange={this.handleDescriptionChange}></textarea>
           <div className="btn-container">
--- client/src/board/NewCard.js
+++ client/src/board/NewCard.js
@@ -41,7 +41,9 @@ class NewCard extends React.Component {
                     onChange={this.handleTitleChange} onKeyUp={this.handleKeyUpTitle} />
                 <div className="new-card-buttons">
                     <button className="primary-action-btn add" onClick={this.handleAddCard}>Add card</button>
-                    <div className="abort-creation" onClick={this.props.abort}><span>X</span></div>
+                    <div className="abort-creation" onClick={this.props.abort}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#333333AA"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
+                    </div>
                 </div>
             </div>
         );
